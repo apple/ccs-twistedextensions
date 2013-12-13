0a42ac1c9d8b7f626bf7200ef80df70e646b8fde
Wilfredo Sanchez <wsanchez@apple.com>
2013-12-13 03:03:13 +0000

    Add preamble for scripts.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/twext/trunk@12080 e27351fd-9f3e-4f54-a53b-843176b1656c

---
+++ bin/_twext_preamble.py
@@ -0,0 +1,22 @@
+##
+# Copyright (c) 2011-2013 Apple Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
+"""
+This module is the shared preamble for all calendarserver shell commands to set
+up their environment properly.  It's explicitly not installed along with the
+code, and is used only to initialize the environment for a development checkout
+of the code.
+"""
