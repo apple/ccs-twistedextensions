c3dec287b2ac2f21ebc34043ea2f30d8afdb6cb9
Wilfredo Sanchez <wsanchez@apple.com>
2013-12-13 02:14:09 +0000

    Add setup.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver/twext/trunk@12077 e27351fd-9f3e-4f54-a53b-843176b1656c

---
+++ setup.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+##
+# Copyright (c) 2013 Apple Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+from __future__ import print_function
+
+import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "support"))
+
+from version import version
+
+
+#
+# Options
+#
+
+description = "Extentions to Twisted",
+long_description = """
+Extentions to the Twisted Framework (http://twistedmatrix.com/).
+"""
+
+classifiers = None
+
+
+#
+# Write version file
+#
+
+version_number, version_info = version()
+
+version_string = (
+    "{number} ({info})"
+    .format(number=version_number, info=version_info)
+)
+version_file = file(os.path.join("twext", "version.py"), "w")
+version_file.write('version = "{version}"\n'.format(version=version_string))
+version_file.close()
+
+
+#
+# Set up Extension modules that need to be built
+#
+
+from distutils.core import Extension
+
+extensions = [
+    Extension("twext.python.sendmsg", sources=["twext/python/sendmsg.c"])
+]
+
+if sys.platform == "darwin":
+    from twext.python import launchd
+    extensions.append(launchd.ffi.verifier.get_extension())
+
+
+#
+# Run setup
+#
+
+def doSetup():
+    from distutils.core import setup
+
+    setup(
+        name="twext",
+        version=version_string,
+        description=description,
+        long_description=long_description,
+        url="http://trac.calendarserver.org/wiki/twext",
+        classifiers=classifiers,
+        author="Apple Inc.",
+        author_email=None,
+        license="Apache License, Version 2.0",
+        platforms=["all"],
+        packages=["twext"],
+        package_data={},
+        scripts=[],
+        data_files=[],
+        ext_modules=extensions,
+        py_modules=[],
+    )
+
+
+#
+# Main
+#
+
+if __name__ == "__main__":
+    doSetup()
---
+++ support/version.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+##
+# Copyright (c) 2006-2013 Apple Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
+from __future__ import print_function
+
+from os.path import dirname
+import subprocess
+
+
+
+def version():
+    #
+    # Compute the version number.
+    #
+
+    base_version = "0.1"
+
+    branches = tuple(
+        branch.format(
+            project="twext",
+            version=base_version,
+        )
+        for branch in (
+            "tags/release/{project}-{version}",
+            "branches/release/{project}-{version}-dev",
+            "trunk",
+        )
+    )
+
+    source_root = dirname(dirname(__file__))
+
+    for branch in branches:
+        svn_revision = subprocess.check_output(
+            ["svnversion", "-n", source_root, branch]
+        )
+
+        if "S" in svn_revision:
+            continue
+
+        if branch == "trunk":
+            base_version += "-trunk"
+        elif branch.endswith("-dev"):
+            base_version += "-dev"
+
+        if svn_revision in ("exported", "Unversioned directory"):
+            comment = "unknown"
+        else:
+            comment = "r{revision}".format(revision=svn_revision)
+
+        break
+    else:
+        base_version += "-unknown"
+        comment = "r{revision}".format(revision=svn_revision)
+
+    return (base_version, comment)
+
+
+
+if __name__ == "__main__":
+    base_version, comment = version()
+    print(
+        "{version} ({comment})"
+        .format(version=base_version, comment=comment)
+    )
